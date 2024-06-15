df5547df543a570f8f3f032ea815ef00e020d147
Faruk Imamovic <f.imamovic27@gmail.com>
2024-06-15 14:44:58 +0200

    [30m]: initial commit, made book model & API, filled with fake data

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'author' => fake()->name(),
+            'description' => fake()->paragraph(),
+            'publication_year' => fake()->year(),
+            'cover_image' => fake()->imageUrl(),
+        ];
+    }
+}
