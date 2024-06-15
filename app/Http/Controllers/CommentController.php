30fbdc2b8612c6415e42c7c4c03a34c9882635b0
Faruk Imamovic <f.imamovic27@gmail.com>
2024-06-15 14:57:36 +0200

    [10m]: basic comment skeleton

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comment $note)
+    {
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $note)
+    {
+
+
+    }
+}
